90a4d8318cbd2b57ccb6e4af6948f197f2a2fa44
Lokesh156 <S555156@nwmissouri.edu>
2022-11-11 16:12:59 -0600

    lab 12 commit

--- controllers/forest.js
+++ controllers/forest.js
@@ -10,10 +10,16 @@ exports.forest_list = async function(req, res) {
         res.send(`{"error": ${err}}`);
         } 
 };
-// for a specific forest.
-exports.forest_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Forest detail: ' + req.params.id);
-};
+exports.forest_detail = async function(req, res) {
+  console.log("detail" + req.params.id)
+  try {
+  result = await Forest.findById( req.params.id)
+  res.send(result)
+  } catch (error) {
+  res.status(500)
+  res.send(`{"error": document for id ${req.params.id} not found`);
+  }
+ };
 // Handle forest create on POST.
 // Handle forest create on POST.
 exports.forest_create_post = async function(req, res) {
@@ -22,7 +28,7 @@ exports.forest_create_post = async function(req, res) {
   // We are looking for a body, since POST does not have query parameters.
   // Even though bodies can be in many different formats, we will be picky
   // and require that it be a json object
-  // {"forest_type":"goat", "cost":12, "size":"large"}
+  // {"forest_area":"goat", "forest_color":12, "forest_dense":"large"}
   document.forest_area = req.body.forest_area;
   document.forest_color = req.body.forest_color;
   document.forest_dense = req.body.forest_dense;
@@ -56,3 +62,23 @@ exports.forest_view_all_Page = async function(req, res) {
   res.send(`{"error": ${err}}`);
   }
  };
+
+ // Handle forest update form on PUT.
+exports.forest_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await Forest.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.forest_area) toUpdate.forest_area = req.body.forest_area;
+ if(req.body.forest_color) toUpdate.forest_color = req.body.forest_color;
+ if(req.body.forest_dense) toUpdate.forest_dense = req.body.forest_dense;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
