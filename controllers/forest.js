81a8f873c604423460d70535b87cb14e03ef3ddb
Lokesh156 <S555156@nwmissouri.edu>
2022-11-11 00:26:11 -0600

    Update forest.js

--- controllers/forest.js
+++ controllers/forest.js
@@ -15,9 +15,26 @@ exports.forest_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Forest detail: ' + req.params.id);
 };
 // Handle forest create on POST.
-exports.forest_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Forest create POST');
-};
+// Handle forest create on POST.
+exports.forest_create_post = async function(req, res) {
+  console.log(req.body)
+  let document = new Forest();
+  // We are looking for a body, since POST does not have query parameters.
+  // Even though bodies can be in many different formats, we will be picky
+  // and require that it be a json object
+  // {"forest_type":"goat", "cost":12, "size":"large"}
+  document.forest_area = req.body.forest_area;
+  document.forest_color = req.body.forest_color;
+  document.forest_dense = req.body.forest_dense;
+  try{
+  let result = await document.save();
+  res.send(result);
+  }
+  catch(err){
+  res.status(500);
+  res.send(`{"error": ${err}}`);
+  }
+ };
 // Handle forest delete form on DELETE.
 exports.forest_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Forest delete DELETE ' + req.params.id);
@@ -26,3 +43,16 @@ exports.forest_delete = function(req, res) {
 exports.forest_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: Forest update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.forest_view_all_Page = async function(req, res) {
+  try{
+  theForests = await Forest.find();
+  res.render('forest', { title: 'forest Search Results', result: theForests });
+  }
+  catch(err){
+  res.status(500);
+  res.send(`{"error": ${err}}`);
+  }
+ };
