d7529a08477f374e0347c0b75acc742401a95e3d
Lokesh156 <S555156@nwmissouri.edu>
2022-11-16 17:47:02 -0600

    Update forest.js

--- routes/forest.js
+++ routes/forest.js
@@ -3,4 +3,12 @@ const forest_controlers= require('../controllers/forest');
 var router = express.Router();
 /* GET forests */
 router.get('/', forest_controlers.forest_view_all_Page );
+/* GET detail forest page */
+router.get('/detail', forest_controlers.forest_view_one_Page);
+/* GET create forest page */
+router.get('/create', forest_controlers.forest_create_Page);
+/* GET create update page */
+router.get('/update', forest_controlers.forest_update_Page);
+/* GET delete forest page */
+router.get('/delete', forest_controlers.forest_delete_Page);
 module.exports = router;
