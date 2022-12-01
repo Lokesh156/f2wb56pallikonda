435fc1b6fa0571967332ab632bfaced71319912a
Lokesh156 <S555156@nwmissouri.edu>
2022-11-30 21:16:57 -0600

    Update forest.js

--- routes/forest.js
+++ routes/forest.js
@@ -1,6 +1,15 @@
 var express = require('express');
-const forest_controlers= require('../controllers/forest');
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 var router = express.Router();
+var forest_controlers= require('../controllers/forest');
 /* GET forests */
 router.get('/', forest_controlers.forest_view_all_Page );
 /* GET detail forest page */
@@ -8,7 +17,7 @@ router.get('/detail', forest_controlers.forest_view_one_Page);
 /* GET create forest page */
 router.get('/create', forest_controlers.forest_create_Page);
 /* GET create update page */
-router.get('/update', forest_controlers.forest_update_Page);
+router.get('/update',secured, forest_controlers.forest_update_Page);
 /* GET delete forest page */
 router.get('/delete', forest_controlers.forest_delete_Page);
 module.exports = router;
