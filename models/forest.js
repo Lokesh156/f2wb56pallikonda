d57f86c30a1a03bc3383019a0a405f9b6a8b5265
Lokesh156 <S555156@nwmissouri.edu>
2022-11-30 21:16:51 -0600

    Update forest.js

--- models/forest.js
+++ models/forest.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const forestSchema = mongoose.Schema({
 forest_area: String,
 forest_color: String,
-forest_dense: String
+forest_dense:Number,
 })
 module.exports = mongoose.model("Forest",
 forestSchema)
